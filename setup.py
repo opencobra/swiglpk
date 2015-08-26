e9c870cf4f0859f0c9b57b80743f4cfa3f7ddc45
Nikolaus Sonnenschein <niko.sonnenschein@gmail.com>
2015-08-26 21:42:22 +0200

    Deploy sdist from travis

--- setup.py
+++ setup.py
@@ -56,7 +56,7 @@ except (IOError, ImportError):
 
 setup(
     name='swiglpk',
-    version='1.0.9',
+    version='1.0.10',
     author='Nikolaus Sonnenschein',
     author_email='niko.sonnenschein@gmail.com',
     description='swiglpk - Simple swig bindings for the GNU Linear Programming Kit',
