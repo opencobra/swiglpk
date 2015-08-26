c9e1d2498a9e483f3996e784dc92b4c9f2f05d56
Nikolaus Sonnenschein <niko.sonnenschein@gmail.com>
2015-08-26 18:45:01 +0200

    Use twine to upload to phypi

--- setup.py
+++ setup.py
@@ -56,7 +56,7 @@ except (IOError, ImportError):
 
 setup(
     name='swiglpk',
-    version='1.0.4',
+    version='1.0.5',
     author='Nikolaus Sonnenschein',
     author_email='niko.sonnenschein@gmail.com',
     description='swiglpk - Simple swig bindings for the GNU Linear Programming Kit',
