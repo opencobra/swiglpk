cce84e356123500b130abd998d47323207846aca
Nikolaus Sonnenschein <niko.sonnenschein@gmail.com>
2015-08-26 21:12:30 +0200

    Fix twine path in powershell script

--- setup.py
+++ setup.py
@@ -56,7 +56,7 @@ except (IOError, ImportError):
 
 setup(
     name='swiglpk',
-    version='1.0.8',
+    version='1.0.9',
     author='Nikolaus Sonnenschein',
     author_email='niko.sonnenschein@gmail.com',
     description='swiglpk - Simple swig bindings for the GNU Linear Programming Kit',
