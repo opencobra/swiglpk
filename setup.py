fabfeaf08a73043beee0c651395b7b16a2805036
Nikolaus Sonnenschein <niko.sonnenschein@gmail.com>
2015-09-02 18:58:04 +0200

    Release 1.2.1

--- setup.py
+++ setup.py
@@ -61,7 +61,7 @@ except (IOError, ImportError):
 
 setup(
     name='swiglpk',
-    version='1.2.0',
+    version='1.2.1',
     author='Nikolaus Sonnenschein',
     author_email='niko.sonnenschein@gmail.com',
     description='swiglpk - Simple swig bindings for the GNU Linear Programming Kit',
