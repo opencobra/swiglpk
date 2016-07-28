88a23ffdd70e547797b4ae14b78c7f7d2708350b
Henning Redestig <henning.red@gmail.com>
2016-07-28 17:14:38 +0200

    deploy for all branches
    can't figure out how to make deploy trigger for only one branch
    - deploy stage doesn't work with many linux wheels
    - deploy script stage doesn't work with mac
    - travis_branch env var isn't set properly by travis

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 set -e
-GIT_BRANCH=`git branch --color=never | grep '*' | cut -d' ' -f2`
-if [[ -n "$TRAVIS_TAG" && "$GIT_BRANCH" == "feature/auto-wheel" ]]; then
+
+# seemingly bug in travis: TRAVIS_BRANCH doesn't get set to the branch but to the tag
+
+if [[ -n "$TRAVIS_TAG" ]]; then
 	echo -e " ... running twine to deploy ... "
 	pip install twine
 	twine upload --skip-existing --username "${PYPI_USERNAME}" --password "${PYPI_PASSWORD}" ${TRAVIS_BUILD_DIR}/wheelhouse/*
 else
-	echo -e " ... skipping deploy as no tag detected: $TRAVIS_TAG - $TRAVIS_BRANCH - $GIT_BRANCH ... "
+	echo -e " ... skipping deploy as no tag detected: $TRAVIS_TAG ... "
 fi
 exit 0;
