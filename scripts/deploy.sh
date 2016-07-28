3e9c7abb47619bc558d7213240af9a7e96b0441a
Henning Redestig <henning.red@gmail.com>
2016-07-28 13:37:51 +0200

    deploy script doesn't work for mac

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 set -e
-echo -e " ... running twine to deploy ... "
-pip install twine
-twine upload --skip-existing --username "${PYPI_USERNAME}" --password "${PYPI_PASSWORD}" ${TRAVIS_BUILD_DIR}/wheelhouse/*
+if [[ -n "$TRAVIS_TAG" && "$TRAVIS_BRANCH" == "feature/auto-wheel" ]]; then
+	echo -e " ... running twine to deploy ... "
+	pip install twine
+	twine upload --skip-existing --username "${PYPI_USERNAME}" --password "${PYPI_PASSWORD}" ${TRAVIS_BUILD_DIR}/wheelhouse/*
+else
+	echo -e " ... skipping deploy as no tagged detected: $TRAVIS_TAG - $TRAVIS_BRANCH ... "
+fi
+exit 0;
