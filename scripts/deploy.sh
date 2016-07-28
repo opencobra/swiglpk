e6a3c2db2732ed3fa22976ae29dd3afa2dec5b70
Henning Redestig <henning.red@gmail.com>
2016-07-28 10:48:24 +0200

    updated pwd

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,5 +2,4 @@
 set -e
 echo -e " ... running twine to deploy ... "
 pip install twine
-echo -e "twine upload --skip-existing --username \"${PYPI_USERNAME}\" --password \"${PYPI_PASSWORD}\" ${TRAVIS_BUILD_DIR}/wheelhouse/*"
-#twine upload --skip-existing --username "${PYPI_USERNAME}" --password "${PYPI_PASSWORD}" ${TRAVIS_BUILD_DIR}/wheelhouse/*
+twine upload --skip-existing --username "${PYPI_USERNAME}" --password "${PYPI_PASSWORD}" ${TRAVIS_BUILD_DIR}/wheelhouse/*
