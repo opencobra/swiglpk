af9b2e46bb035ad3c2ea43303fb31bd6fb4e63ae
Henning Redestig <henning.red@gmail.com>
2016-07-28 15:10:19 +0200

    TRAVIS_BRANCH doesn't get set as it should

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 set -e
-if [[ -n "$TRAVIS_TAG" && "$TRAVIS_BRANCH" == "feature/auto-wheel" ]]; then
+GIT_BRANCH=`git branch --color=never | grep '*' | cut -d' ' -f2`
+if [[ -n "$TRAVIS_TAG" && "$GIT_BRANCH" == "feature/auto-wheel" ]]; then
 	echo -e " ... running twine to deploy ... "
 	pip install twine
 	twine upload --skip-existing --username "${PYPI_USERNAME}" --password "${PYPI_PASSWORD}" ${TRAVIS_BUILD_DIR}/wheelhouse/*
 else
-	echo -e " ... skipping deploy as no tag detected: TRAVIS_TAG $TRAVIS_TAG - TRAVIS_BRANCH $TRAVIS_BRANCH ... "
+	echo -e " ... skipping deploy as no tag detected: $TRAVIS_TAG - $TRAVIS_BRANCH - $GIT_BRANCH ... "
 fi
 exit 0;
