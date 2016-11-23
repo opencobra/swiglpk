aaacd40f4377cdd920f57e9be091c781c7eb735f
Christian Diener <ch.diener@gmail.com>
2016-11-23 15:09:03 -0600

    Remove unused files

--- appveyor/deploy_to_pypi.ps1
+++
@@ -1,6 +0,0 @@
-if($env:appveyor_repo_tag -eq 'True') {
-    "echo [pypi] > %USERPROFILE%\\.pypirc"
-    "echo username: Nikolaus.Sonnenschein >> %USERPROFILE%\\.pypirc"
-    "echo password: %password% >> %USERPROFILE%\\.pypirc"
-    "%WITH_COMPILER% %PYTHON%/python setup.py bdist_wheel sdist upload"
-}
