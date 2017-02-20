cf50b98cd382a050654550debece6445ca9a21a6
Henning Redestig <henning.red@gmail.com>
2017-02-20 09:38:38 +0100

    fix: use regular glpk (#21)
    * fix: use regular glpk
    - cobrapy use regular glpk - quite possibly cause for random crashes.
    - upgrade glpk to 4.61
    * only build pull requests and master devel
    when building all branches, every pull request on branch within the main
    repo triggers two identical builds, the base after merge and the
    branch (which is base after merge).
    * same on appveyor

--- config.sh
+++ config.sh
@@ -7,35 +7,34 @@ function pre_build {
     if [ -n "$IS_OSX" ]; then
         export CC=clang
         export CXX=clang++
+        export CFLAGS="-fPIC -O3 -arch i386 -arch x86_64 -g -DNDEBUG -mmacosx-version-min=10.6"
         brew tap homebrew/science
         brew update
-        brew install glpk swig # automake
+        brew install swig # automake
     else
-        curl -O http://ftp.gnu.org/gnu/glpk/glpk-4.60.tar.gz
-        tar xzf glpk-4.60.tar.gz
-        (cd glpk-4.60 \
-        && ./configure --prefix=$BUILD_PREFIX \
-        && make \
-        && make install)
-
         yum install -y pcre-devel
 		# yum install automake
         curl -O -L http://downloads.sourceforge.net/swig/swig-3.0.10.tar.gz
         tar xzf swig-3.0.10.tar.gz
         (cd swig-3.0.10 \
-        && ./configure --prefix=$BUILD_PREFIX \
-        && make \
-        && make install)
- 		# git clone https://github.com/swig/swig.git
-        # (cd swig \
-		# 		&& git checkout rel-3.0.10 \
-		# 		&& ./autogen.sh \
-		# 		&& ./configure --prefix=$BUILD_PREFIX \
-		# 		&& make \
-		# 		&& make install)
-    fi
+				&& ./configure --prefix=$BUILD_PREFIX \
+				&& make \
+				&& make install)
+	fi
+    curl -O http://ftp.gnu.org/gnu/glpk/glpk-4.61.tar.gz
+    tar xzf glpk-4.61.tar.gz
+    (cd glpk-4.61 \
+            && ./configure --disable-reentrant --prefix=$BUILD_PREFIX \
+            && make \
+            && make install)
 
-
+ 	# git clone https://github.com/swig/swig.git
+    # (cd swig \
+	# 		&& git checkout rel-3.0.10 \
+	# 		&& ./autogen.sh \
+	# 		&& ./configure --prefix=$BUILD_PREFIX \
+	# 		&& make \
+	# 		&& make install)
 }
 
 function build_wheel {
@@ -48,12 +47,7 @@ function run_tests {
     export NOSE_PROCESS_TIMEOUT=600
     export NOSE_PROCESSES=0
     echo "OS X? $IS_OSX"
-    if [ -n "$IS_OSX" ]; then
-        echo "uninstalling glpk for tests"
-        brew uninstall -y glpk  # remove glpk to make sure that the OS X wheel works standalone.
-    else
-        rm -f /usr/local/lib/libglpk*
-    fi
+    rm -f /usr/local/lib/libglpk*
     # Run Pillow tests from within source repo
     cp ../test_swiglpk.py .
     nosetests -v
