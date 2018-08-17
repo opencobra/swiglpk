9950c2d301a5035004420bee907a4296e539e110
Nikolaus Sonnenschein <niko.sonnenschein@gmail.com>
2018-08-17 14:26:41 +0200

    wip: get ready for move to glpk versioning

--- scripts/find-newest-glpk-release.py
+++ scripts/find-newest-glpk-release.py
@@ -1,11 +1,37 @@
 import sys
-import git
 import requests
+import json
+try:
+    from packaging.version import parse
+except ImportError:
+    from pip._vendor.packaging.version import parse
 
-g = git.Git('.')
-current_swiglpk_tag = g.describe()
-major, minor = current_swiglpk_tag.split('.')
+
+URL_PATTERN = 'https://pypi.python.org/pypi/{package}/json'
+
+
+def get_version(package, url_pattern=URL_PATTERN):
+    """Return version of package on pypi.python.org using json. Adapted from https://stackoverflow.com/a/34366589"""
+    req = requests.get(url_pattern.format(package=package))
+    version = parse('0')
+    if req.status_code == requests.codes.ok:
+        # j = json.loads(req.text.encode(req.encoding))
+        j = req.json()
+        releases = j.get('releases', [])
+        for release in releases:
+            ver = parse(release)
+            if not ver.is_prerelease:
+                version = max(version, ver)
+    return version
+
+version = get_version('swiglpk')
+print(version)
+major, minor = version.public.split('.')[0:2]
+print(major)
+print(minor)
 major, minor = int(major), int(minor)
+print(major)
+print(minor)
 
 request = requests.head('http://ftp.gnu.org/gnu/glpk/glpk-{}.{}.tar.gz'.format(major, minor+1))
 if request.status_code == 200:
