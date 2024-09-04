33c8f3a4e2c2b688812cd20a03ec7f7420f0d216
Christian Diener <ch.diener@gmail.com>
2024-09-04 08:46:32 +0200

    try with best guess and fallback

--- scripts/build_glpk.py
+++ scripts/build_glpk.py
@@ -1,8 +1,6 @@
 # taken from https://github.com/opencobra/cobrapy/blob/devel/appveyor/build_glpk.py
 
 import os
-import sys
-# import hashlib
 import tarfile
 import struct
 import shutil
@@ -10,33 +8,27 @@ from setuptools.msvc import EnvironmentInfo
 import urllib.request as urllib2
 import subprocess
 
+GUESS_VCVARS = (
+    "C:\\Program Files\\Microsoft Visual Studio\\2022\\Enterprise\\"
+    "VC\\Auxiliary\\Build\\vcvarsall.bat"
+)
+
 # these need to be set to the latest glpk version
 glpk_version = os.getenv('NEW_GLPK_VERSION')
-# glpk_md5 = "eda7965907f6919ffc69801646f13c3e"
 
 glpk_build_dir = "glpk_build/glpk-%s" % glpk_version
 url = "http://ftp.gnu.org/gnu/glpk/glpk-%s.tar.gz" % glpk_version
 bitness = struct.calcsize("P") * 8
 arch = "x86_amd64" if bitness == 64 else "x86"
 
-
-# def md5(fname):
-#     hash = hashlib.md5()
-#     with open(fname, "rb") as f:
-#         for chunk in iter(lambda: f.read(4096), b""):
-#             hash.update(chunk)
-#     return hash.hexdigest()
-
 def find_vcvarsall():
-    candidates = []
+    if os.path.isfile(GUESS_VCVARS):
+        return(GUESS_VCVARS)
     for root, dirs, files in os.walk("C:\\Program Files\\Microsoft Visual Studio\\"):
-        found = [
-            os.path.join(root, *dirs, f)
-            for f in files
-            if f == "vcvarsall.bat"
-        ]
-        candidates.extend(found)
-    return candidates
+        for f in files:
+            if f == "vcvarsall.bat":
+                return(os.path.join(root, *dirs, f)
+    raise RuntimeError("Could not find vcvarsall.bat :(")
 
 
 if not os.path.isdir("glpk_build/"):
@@ -49,18 +41,12 @@ if not os.path.isdir(glpk_build_dir):
     with tarfile.open("glpk-download.tar.gz") as infile:
         infile.extractall("glpk_build")
 
-vcv = find_vcvarsall()
-print(vcv)
-
 os.chdir("%s/w%d" % (glpk_build_dir, bitness))
 if not os.path.isfile("glpk.lib"):
     shutil.copy2("config_VC", "config.h")
-    env = EnvironmentInfo(arch).return_env()
-    print(env)
-    vcvars = f'"C:\\Program Files\\Microsoft Visual Studio\\2022\\Enterprise\\VC\\Auxiliary\\Build\\vcvarsall.bat" {arch}'
-    subprocess.run(vcvars, check=True, shell=True)
+    vc_setup = find_vcvarsall()
     subprocess.run(
-        f"{vcvars} & nmake /f Makefile_VC",
+        f'"{vcvars}" {arch} & nmake /f Makefile_VC',
         check=True, shell=True
     )
 shutil.copy2("glpk.lib", "../../..")
